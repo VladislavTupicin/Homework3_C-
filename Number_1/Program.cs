7968690552a1dde8d476860239e9dad32a1447c9
«Vlad» <>
2022-09-04 06:03:19 +0300

    Homework3

---
+++ Number_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 19
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(anyNumber);
+if (number.Length == 5)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число {anyNumber} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Ваше число {anyNumber} не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Это не пятизначное число!!!");
+}
---
+++ Number_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 23
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <= n)
+{
+    Console.Write($"{Math.Pow(count, 3)}  ");
+    count ++;
+}
+Console.WriteLine();
